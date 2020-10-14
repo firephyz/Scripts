ec3f1cb572af7f44096df1e782b6fae422c6b79f
Kyle Burge <kyle.burge7196@gmail.com>
2020-10-14 04:16:20 -0400

    Updated binutils build script to work with rpmbuild.

--- toolchain-build-scripts/binutils-build.sh
+++ toolchain-build-scripts/binutils-build.sh
@@ -1,32 +1,53 @@
-PREFIX="/home/kyle/programs/arm-none-eabi/"
-SRC_DIR=$(dirname ${BASH_SOURCE})
-cd $SRC_DIR && cd ../
+#PREFIX="/home/kyle/programs/arm-none-eabi/"
+#SRC_DIR=$(dirname ${BASH_SOURCE})
+#cd $SRC_DIR && cd ../
+# ${prefix}lib
 
-if [[ ($1 -eq 0) || ("x${1}" == "x") ]]
-then
-    old_build_items=$(ls -1 | grep -v _keep | tr '\n' ' ')
-    if [[ ! ("x${old_build_items}" == "x") ]]
+prefix=
+sourcedir=
+action=
+rest=
+
+curflag=
+for arg in $@
+do
+    if [[ ("x${curflag}" == "x") ]]
     then
-        rm -r ${old_build_items}
+        if [[ ("${arg::1}" == "-") ]]
+        then
+            curflag=${arg/-/}
+        else
+            rest="${rest} ${arg}"
+        fi
+    else
+        declare -n curflag
+        curflag=$arg
+        declare +n curflag
+        curflag=
     fi
+done
 
-    # threads, initfini, libiberty
-    ../binutils-2.32/configure \
-        --prefix=$PREFIX \
+prefix=$(readlink -f $prefix)
+sourcedir=$(readlink -f $sourcedir)
+
+if [[ ("${action}" == "configure") ]]
+then
+    ${sourcedir}/configure \
+        --prefix=${prefix} \
         --target=arm-none-eabi \
-        --with-lib-path="${PREFIX}lib/gcc/arm-none-eabi/11.0.0:${PREFIX}arm-none-eabi/lib:${PREFIX}lib" \
+        --with-lib-path="${prefix}lib/gcc/arm-none-eabi/11.0.0:${prefix}arm-none-eabi/lib" \
         --disable-nls
 
-
-elif [[ ($1 -eq 1) ]]
+elif [[ ("${action}" == "build") ]]
 then
     num_threads=9
-    if [[ ! ("x${2}" == "x") ]]
+    if [[ ! ("x${rest}" == "x") ]]
     then
-        num_threads=$2
+        num_threads=${action}
     fi
     make -j${num_threads}
-elif [[ ($1 -eq 2) ]]
+
+elif [[ (${action} -eq "install") ]]
 then
     make install
 fi
